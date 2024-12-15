8dd86b7daf848e8e742fe9a74bebadbcb43b3883
AlexandroGSM <47326209+AlexandroGSM@users.noreply.github.com>
2024-12-15 15:07:43 -0300

    Fix variables

--- Proyecto/variables.tf
+++ Proyecto/variables.tf
@@ -1,4 +1,17 @@
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
+variable "AWS_ACCESS_KEY_ID" {
+  description = "Access Key for AWS"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "Secret Access Key for AWS"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_DEFAULT_REGION" {
+  description = "AWS Default Region"
+  type        = string
+  default     = "us-east-1"
 }
