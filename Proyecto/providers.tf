797a06482083c2fe0771f4f562385349a5d081be
AlexandroGSM <47326209+AlexandroGSM@users.noreply.github.com>
2024-12-15 15:09:02 -0300

    Fix variables y provider

--- Proyecto/providers.tf
+++ Proyecto/providers.tf
@@ -16,5 +16,7 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region     = var.AWS_DEFAULT_REGION
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
